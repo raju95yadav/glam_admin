666e12dc1aeb93d89757b5b7b6b345054e0c812e
raju95yadav <rajuyadavsky489.com>
2026-04-18 01:40:11 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://glam-back-one.vercel.app/api',
 });
 
 // Interceptor to add token to headers
